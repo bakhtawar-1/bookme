9c3ce9c67f7fdef51dbb4510bb43551f8efb165f
bakhtawar-1 <168338810+bakhtawar-1@users.noreply.github.com>
2025-06-28 17:36:10 +0500

    Delete server directory

--- server/api/bookings.ts
+++
@@ -1,7 +0,0 @@
-import { readFileSync } from 'fs'
-import { join } from 'path'
- 
-export default defineEventHandler(() => {
-  const data = readFileSync(join(process.cwd(), 'public/bookings.json'), 'utf-8')
-  return JSON.parse(data)
-}) 
